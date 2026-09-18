c95a52d42d33fc7111737489de042cfeb05853b9
Jordan Mays <jmays12@gmail.com>
2026-09-17 19:10:01 -0500

    Add basic routing to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Dashboard from './pages/Dashboard'
+import Tickets from './pages/Tickets'
+import Customers from './pages/Customers'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/tickets" element={<Tickets />} />
+                <Route path="/customers" element={<Customers />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
 export default App
+
