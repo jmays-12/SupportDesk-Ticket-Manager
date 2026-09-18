6676c0da26cce542c4de4905896b2ceef9692fa6
Jordan Mays <jmays12@gmail.com>
2026-09-17 19:46:44 -0500

    Add Auth routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import Dashboard from './pages/Dashboard'
 import Tickets from './pages/Tickets'
 import Customers from './pages/Customers'
+import Auth from './pages/Auth'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
+                <Route path="/" element={<Auth />} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/tickets" element={<Tickets />} />
                 <Route path="/customers" element={<Customers />} />
             </Routes>
@@ -17,4 +18,3 @@ function App() {
 }
 
 export default App
-
