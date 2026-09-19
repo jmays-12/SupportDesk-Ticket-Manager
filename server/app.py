789769d5b0cfcd47a52abcbdad2d1969d4ffe553
Jordan Mays <jmays12@gmail.com>
2026-09-19 12:42:10 -0500

    Add schemas.py and add basic initial setup for implementing Marshmallow

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_marshmallow import Marshmallow
 
 from models import db, User, Customer, Ticket, TicketNote
 
@@ -13,6 +14,7 @@ from models import db, User, Customer, Ticket, TicketNote
 load_dotenv()
 
 app = Flask(__name__)
+ma = Marshmallow(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
---
+++ server/schemas.py
@@ -0,0 +1,4 @@
+from marshmallow import fields
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+from models import db, User, Customer, Ticket, TicketNote
