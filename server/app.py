42dc96ae6273087f1110d0b04a3fe382785b736f
Jordan Mays <jmays12@gmail.com>
2026-09-19 02:10:20 -0500

    Add tickets GET route

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
 
-from models import db, User, Customer
+from models import db, User, Customer, Ticket
 
 
 load_dotenv()
